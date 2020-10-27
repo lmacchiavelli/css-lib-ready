0a0ff89d1f106452f526ec05b5f333e01895eb94
lmacchiavelli <lmacchiavelli@gmail.com>
2020-10-27 15:48:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import os
 listtree = os.listdir("./Elementi")
 listtree.remove('.DS_Store')
 
+
+from os import listdir
+from os.path import isfile, join
+
+onlyfiles = [f for f in listdir('./Elementi/form') if isfile(join('./Elementi/form', f))]
+
+
+
 def mostraAlbero():
 	a = 0
 	for i in listtree:
 		print(a,i)
+		print(onlyfiles)
 		a+=1
 
 	x = input('Seleziona sezione da inserire nel css:')
-	listtree.pop(int(x))
+	#listtree.pop(int(x))
+	
 	mostraAlbero()
 
 
